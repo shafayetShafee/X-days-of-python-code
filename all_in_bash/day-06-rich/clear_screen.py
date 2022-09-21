ae39ed1f1944e6493c79b876fbd00f8e04a4638b
shafayetShafee <seaborn.sicario@gmail.com>
2022-09-22 00:54:29 +0600

    remove the deleted cache

--- all_in_bash/day-06-rich/clear_screen.py
+++
@@ -1,9 +0,0 @@
-"""
-This script contains clear function for clearing the command line prompt screen
-"""
-
-import os
-
-def clear() -> None:
-    """Runs `cls` command for linux or runs `clear` command in windows"""
-    os.system('cls' if os.name=='nt' else 'clear')
