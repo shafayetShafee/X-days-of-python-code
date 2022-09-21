7222dbee3639d8c880aa0862325243a10f6f9806
shafayetShafee <seaborn.sicario@gmail.com>
2022-09-21 23:42:13 +0600

    added help description to bandname bash file

--- all_in_bash/apps/bandname/bandname.sh
+++ all_in_bash/apps/bandname/bandname.sh
@@ -1,6 +1,35 @@
 #! /bin/bash
 
+############################################################
+# Help                                                     #
+############################################################
+
+Help()
+{
+	# display help
+	echo "Run the app 'bandname-generator' from bash"
+	echo "This app will prompt to you for your city name and pet name"
+	echo "and then combine those name to create a band name for you."
+	echo
+	echo "Syntax: bash bandname.sh [--help|--rich]"
+	echo "Options:"
+	echo "--help 	Print this help"
+	echo "--rich	Enable the rich formatting for the app on the cli"
+	echo
+}
+
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
+
 case $1 in
+	"--help")
+		Help
+		;;
 	"--rich")
 		python main-rich.py
 		;;
